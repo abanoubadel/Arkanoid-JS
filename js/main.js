a8250b89f3c60bace2c3856cdd983e2a4ceb20d5
Abanoub <abanoub@localhost.localdomain>
2015-12-16 14:44:09 +0200

    edit files

--- js/main.js
+++ js/main.js
@@ -36,10 +36,41 @@ var Layout = function(){
 	};
 };
 
+var Level = function(){
+	this.id;
+	this.score;
+	this.time;
+	this.difficulty;
+	this.shape;
+
+	this.prototype.draw = function() {
+		switch(shape){
+			case 'rect':
+				{
+					for (var i = 0; i < .length; i++) {
+						[i]
+					};
+				}
+				break;
+		}	
+	};
+};
+
+var Window = function(){
+	this.id = 'game';
+	this.width = 400;
+	this.height = 800;
+	this.html = document.createElement('div');
+	this.html.id = this.id;
+	this.html.style.width = this.width;
+	this.html.style.height = this.height;
+};
+
 var Game = function(){
-	var menu = new Menu();
+	// var menu = new Menu();
+	var window = new Window();
 	var layout = new Layout();
-	layout.append( menu.generateHTML() );
+	// layout.append( menu.generateHTML() );
 };
 
 var main = function(){
