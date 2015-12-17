51b95213b081005eeb5c1e7d6b39857c92eb93aa
Abanoub <abanoub@localhost.localdomain>
2015-12-17 13:27:33 +0200

    Add levels shape

--- js/class/config.js
+++ js/class/config.js
@@ -1,14 +1,31 @@
 var Config = {
-	Window:{
-		id: 'name',
-		width: 800,
-		height: 500,
-	},
-	Level:{
-		height: 350 //prefer to be 70% of window height
-	},
-	Block:{
-		width: 85,
-		height: 40,
-	}
+    Window: {
+        id: 'name',
+        width: 1000,
+        height: 500,
+    },
+    Level: {
+    	id: 'levelShape',
+        start: 1,
+        levels: [{
+            time: 5, // in mints
+            difficulty: 0, //
+            shape: 'rect'
+        }, {
+            time: 3, // in mints
+            difficulty: 1, //
+            shape: 'rect'
+        }, {
+            time: 1, // in mints
+            difficulty: 2, //
+            shape: 'rect'
+        }],
+        height: 200, //prefer to be 70% of window height
+        width: 300
+    },
+    Block: {
+        width: 60,
+        height: 25,
+        margin: 5,
+    }
 };
--- js/class/shape.js
+++ js/class/shape.js
@@ -1,114 +1,110 @@
-var Shape = function (x,y,width,height,id,addedClass) {
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
-	this.id = id;
-	this.initialClass = "position";
-	this.addedClass = addedClass;
-	this.generateHtml();
-	
+var Shape = function(x, y, width, height, id, addedClass) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.id = id;
+    this.initialClass = "position";
+    this.addedClass = addedClass;
+    this.generateHtml();
+
 
 };
 
-
-Shape.prototype.getIntoContainer =function(container)
-{
-	this.div = container.appendChild(this.div);
-}
-
-Shape.prototype.getCenter = function(){
-	return [this.x+this.width/2,this.y+this.height/2];
-}
-
-Shape.prototype.getWidth = function(){
-	return this.width;
-}
-
-Shape.prototype.getHeight = function(){
-	return this.height;
+Shape.prototype.addStyle = function(style, value) {
+    this.div.style[style] = value;
 }
 
 
-
-Shape.prototype.getElement = function(){
-	return this.div;
+Shape.prototype.getIntoContainer = function(container) {
+    this.div = container.appendChild(this.div);
 }
 
-Shape.prototype.setPlace = function () {
-		// body...
-		if(this.div)
-		{
-			this.div.style.left = this.x + "px";
-			this.div.style.bottom = this.y + "px";
-		}
+Shape.prototype.getCenter = function() {
+    return [this.x + this.width / 2, this.y + this.height / 2];
 }
-Shape.prototype.addClass = function (cls) {
-		
-		this.div.classList.add(cls);
-		
+
+Shape.prototype.getWidth = function() {
+    return this.width;
+}
+
+Shape.prototype.getHeight = function() {
+    return this.height;
 }
 
 
-Shape.prototype.setColor = function(color)
-{
-	this.div.style.backgroundColor = color;
+
+Shape.prototype.getElement = function() {
+    return this.div;
+}
+
+Shape.prototype.setPlace = function() {
+    // body...
+    if (this.div) {
+        this.div.style.left = this.x + "px";
+        this.div.style.top = this.y + "px";
+    }
+}
+Shape.prototype.addClass = function(cls) {
+
+    this.div.classList.add(cls);
+
+}
+
+
+Shape.prototype.setColor = function(color) {
+    this.div.style.backgroundColor = color;
 
 }
 
 Shape.prototype.generateHtml = function() {
-		this.div = document.createElement("div");
-		this.div.classList.add(this.initialClass);
-		this.addClass(this.initialClass);
-		this.addClass(this.addedClass);
-		this.div.id= this.id;
-		this.div.style.width = this.width+"px";
-		this.div.style.height = this.height+"px";
-		this.setPlace();
+    this.div = document.createElement("div");
+    this.div.classList.add(this.initialClass);
+    this.addClass(this.initialClass);
+    this.addClass(this.addedClass);
+    this.div.id = this.id;
+    this.div.style.width = this.width + "px";
+    this.div.style.height = this.height + "px";
+    this.setPlace();
 };
 
-Shape.prototype.getX = function()
-{
-	return this.x;
+Shape.prototype.getX = function() {
+    return this.x;
 };
 
-Shape.prototype.getY = function()
-{
-	return this.y;
+Shape.prototype.getY = function() {
+    return this.y;
 };
 
-Shape.prototype.getWidth = function()
-{
-	return this.width;
+Shape.prototype.getWidth = function() {
+    return this.width;
 };
 
-Shape.prototype.getHeight = function()
-{
-	return this.height;
+Shape.prototype.getHeight = function() {
+    return this.height;
 };
 
 
 
 
-var MovingShape = function(x,y,width,height,id,addedClass){
-	Shape.call(this,x,y,width,height,id,addedClass);
-	this.velocityX = 0;
-	this.velocityY = 0;
+var MovingShape = function(x, y, width, height, id, addedClass) {
+    Shape.call(this, x, y, width, height, id, addedClass);
+    this.velocityX = 0;
+    this.velocityY = 0;
 
 }
 MovingShape.prototype = Object.create(Shape.prototype);
 MovingShape.prototype.constructor = MovingShape;
 
-MovingShape.prototype.move = function(x,y)
-{
-	this.x+= x;
-	this.y += y;
-	this.setPlace();	
+MovingShape.prototype.move = function(x, y) {
+    this.x += x;
+    this.y += y;
+    this.setPlace();
 }
 
-MovingShape.prototype.setX = function (x) {
-	// body...
-	this.move(x-this.getX(),0);
+MovingShape.prototype.setX = function(x) {
+    // body...
+    this.move(x - this.getX(), 0);
 }
 
 
@@ -119,21 +115,18 @@ MovingShape.prototype.setX = function (x) {
 
 
 
-var Rod = function(id,isVertical,x,y,length){
-	var height;
-	var width;
+var Rod = function(id, isVertical, x, y, length) {
+    var height;
+    var width;
 
-	if(isVertical)
-	{
-		height = length;
-		width = 0;
-	}
-	else
-	{
-		height = 0;
-		width = length;
-	}
-	Shape.call(this,x,y,width,height,id,"");
+    if (isVertical) {
+        height = length;
+        width = 0;
+    } else {
+        height = 0;
+        width = length;
+    }
+    Shape.call(this, x, y, width, height, id, "");
 
 
 }
@@ -143,20 +136,19 @@ Rod.prototype.constructor = Rod;
 
 
 
-var Swing = function(x,y)
-{
-	//calling constructors
-	Shape.call(this,x,y,100,10,"swing","swing");
-	this.setColor("black");
+var Swing = function(x, y) {
+    //calling constructors
+    Shape.call(this, x, y, 100, 10, "swing", "swing");
+    this.setColor("black");
 }
 
 Swing.prototype = Object.create(MovingShape.prototype);
 Swing.prototype.constructor = Swing;
 
-Swing.prototype.move = function (x) {
-	// body...
-	MovingShape.prototype.move.call(this,x,0);
-} 
+Swing.prototype.move = function(x) {
+    // body...
+    MovingShape.prototype.move.call(this, x, 0);
+}
 
 
 
